1bb05b287816c50ff8fcf2619b9303244aca8890
Aditya Raj Panda <aditya.panda@cubastion.com>
2026-04-29 16:42:23 +0530

    Updated 02_main.py file

--- 01_FastAPI/02_main.py
+++ 01_FastAPI/02_main.py
@@ -8,8 +8,24 @@ def test():
 
 @app.get("/aditya")
 def test_1():
-    return "I am a Data Engineer"
+    return {"message": "I am a Data Engineer"}
 
+people = [
+        {"name": "Aditya", "city": "Bangalore", "profession": "Data Engineer"},
+        {"name": "Josh", "city": "Mumbai", "profession": "Data Scientist"},
+        {"name": "Sonia", "city": "Delhi", "profession": "Data Analyst"}
+]
+
+@app.get("/people")
+def get_people():
+    return {"people": people}
+
+# parameterized endpoint to search for a person by name
+@app.get("/people/{name}")
+def people_search(name: str):
+    for person in people:
+        if person["name"].lower() == name.lower():
+            return {"person":person}
 
 # In  FastAPI, Uvicorn is the lightning-fast web server that actually runs your application. While FastAPI defines 
 # the routes, logic, and data validation, it does not have a built-in server to listen for network requests; 
